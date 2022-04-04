a5e393d3d4f002ee9a9ed9b951b16c00148bcd06
iiiw <git@iiiw.dev>
2022-04-04 17:01:38 +0200

    Fix license header test
    Adapt package.yml and test script to text format changes in the spdx repo.

--- test/license_headers_match.pl
+++ test/license_headers_match.pl
@@ -6,11 +6,11 @@ use warnings;
 use 5.010;
 use FindBin;
 
-# Use the modified version of YAML::Tiny from the tools directory.
+# Use the modified version of YAML::Tiny in the tools directory.
 use lib "$FindBin::Bin/../tools/YAML-Tiny/lib";
 use YAML::Tiny;
 
-# Patterns                                                  -- matches              -- captures         -- example          - gives
+# Patterns                                                  -- matches              -- captures         -- example          - result
 my $pat_filename_id = qr/([\w.-]+?)\.txt/;                  # filename              id (== basename)    'Apache-2.0.txt'    'Apache-2.0'
 my $pat_indent      = qr/\{\{(?:t_)?indent(\d{1,2})\}\}/;   # indent placeholder    number of spaces    '{{t_indent4}}'     4
 my $pat_concat      = qr/\{\{t_concat\}\}/;                 # concat marker                             '{{t_concat}}'
@@ -20,44 +20,44 @@ my $pat_year        = qr/\{\{year\}\}/;                     # year placeholder  
 sub main;
 main();
 
-# The script gets the path to the espanso package configuration and tests if
-# the license headers hardcoded in the configuration match their online
-# reference license data.
+# The script gets the path to the package.yml (match file) and tests if the
+# hardcoded license headers match their online counterparts.
 sub main {
     my $path = $ARGV[0];
 
     die <<USAGE if not defined $path;
 $FindBin::Script: not enough arguments
 usage:  $FindBin::Script <path>
-        path    path to package.yml configuration
+        path    path to package.yml file
 USAGE
 
     my $yaml = YAML::Tiny->read($path);
     my @license_headers = grep { $_->{trigger} =~ /^:lh/ } @{$yaml->[0]->{matches}};
-    my @license_texts   = grep { $_->{trigger} =~ /^:lt/ } @{$yaml->[0]->{matches}};
-    my %license_map     = map  { $_->{trigger} => $_     } @license_texts;
+    my @license_texts   = grep { $_->{trigger} =~ /^:l[a-z]{3}\d?$/ } @{$yaml->[0]->{matches}};
+    my %license_map     = map  { $_->{trigger} => $_ } @license_texts;
 
     for (@license_headers) {
-        # Get the license text entry corresponding to the license header entry.
-        ( my $key = $_->{trigger} ) =~ s/^:lh/:lt/;
+        # Get the license text corresponding to the header.
+        ( my $key = $_->{trigger} ) =~ s/^:lh/:l/;
         next unless exists $license_map{$key};
 
         my $entry = $license_map{$key};
         my $id    = license_id($entry);
 
-        # The curl command downloads the full text of the license.
-        my $curl  = curl_cmd($entry);
-        my $str_a = qx( $curl );
+        # Download the full text of the license.
+        my $dl    = curl_cmd($entry);
+        my $str_a = qx( $dl );
         die "calling system curl failed with error: ${^CHILD_ERROR_NATIVE}"
             if ${^CHILD_ERROR_NATIVE};
 
+        # Modify the hardcoded header to get rid of additional markup and whitespace.
         my $str_b = $_->{replace};
         $str_b    = tr_year($str_b);
         $str_b    = tr_concat($str_b);
         $str_b    = tr_indent($str_b);
         $str_b    = trim_cursor($str_b);
 
-        # The license header must be part of the license text.
+        # The license header $str_b must be part of the license text $str_a.
         die <<MESSAGE if index($str_a, $str_b) == -1;
 test failed:
     license header ($_->{trigger}) does not match the $id license ($key)
@@ -72,20 +72,21 @@ sub license_id {
     return $id;
 }
 
-# Get the curl command string.
+# Set up the curl command to download from the spdx repository.
 sub curl_cmd {
     my $id = license_id(shift);
     return "curl -LSs https://raw.githubusercontent.com/" .
-        "spdx/license-list-data/master/text/" . $id . ".txt";
+        "spdx/license-list-data/master/text/" .
+        $id . ".txt";
 }
 
-# Restore line continuations (aka concat lines).
+# Restore line continuations.
 sub tr_concat {
     ( my $str = shift ) =~ s/\s+$pat_concat/ /g;
     return $str;
 }
 
-# Restore indentation ('translate' the indent placeholder).
+# Restore indentation.
 sub tr_indent {
     my $str = shift;
     ( my $cnt ) = ( $str =~ /$pat_indent/ );
@@ -93,7 +94,7 @@ sub tr_indent {
     return $str;
 }
 
-# Restore ('translate') the year placeholder.
+# Restore the year placeholder.
 sub tr_year {
     ( my $str = shift ) =~ s/$pat_year/<year>/g;
     if ($str =~ /Apache License, Version 2\.0/) {
